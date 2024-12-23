58d8afd60f4af36aa5a85e6c40ce0c6f5d0e3b71
bascom241 <135514268+bascom241@users.noreply.github.com>
2024-12-23 15:38:34 +0100

    Seperating the logic

--- tools/Calculator.java
+++ tools/Calculator.java
@@ -1,41 +1,57 @@
 package tools;
 
-import errors.CustomException;
+import errors.*;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Calculator{
+public class Calculator {
     Operators operator = new Operators();
-    String [] actions = operator.getS();
+    String[] actions = operator.getS();
+    Scanner scanner;
     int value1;
     String userOperator;
+    int value2;
 
-    public Calculator(Scanner scanner){
-        try{
-            while(!scanner.hasNext() ){
-                throw new CustomException("Value must be an interger" );
-            }
-            userOperator=scanner.nextLine();
-            if(Arrays.asList(actions).contains(userOperator)){
-
-            }
-           value1 = scanner.nextInt();
-            System.out.println("You entered " + value1);
-
-        }catch(CustomException e){
-            System.out.println(e.getMessage());
-        }
+    public Calculator(Scanner scanner) {
+        this.scanner = scanner;
     }
 
-    public int getValue1(){
+    public int getValue1() {
+        try {
+            while (!scanner.hasNext()) {
+                throw new CustomException("Value must be an interger");
+            }
+            value1 = scanner.nextInt();
+            System.out.println("You entered " + value1);
+            // Get the users Operator and validate //
+
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
         return value1;
     }
 
-    public void calculateResult(){
+    public void validateOperator() {
+
+        try {
+            userOperator = scanner.nextLine();
+            if (!Arrays.asList(actions).contains(userOperator)) {
+               throw new Exception ("Invalid Operator" );
+            }
+            System.out.println("You entered this" + userOperator + "Operator");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 
+    
 
+
+
+    public void calculateResult() {
+
+    }
 
 }
